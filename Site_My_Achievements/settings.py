a274d77f1d25b2f4a899076034a009c9dfe2d7f8
Valemmil <38853350+Valemmil@users.noreply.github.com>
2023-12-11 20:39:15 +0700

    Update settings.py

--- Site_My_Achievements/settings.py
+++ Site_My_Achievements/settings.py
@@ -74,14 +74,21 @@ WSGI_APPLICATION = 'Site_My_Achievements.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/4.2/ref/settings/#databases
 
+# DATABASES = {
+#     'default': {
+#         'ENGINE': 'django.db.backends.postgresql',
+#         'NAME': 'My_Achievements',
+#         'USER': 'postgres',
+#         'PASSWORD': '12345',
+#         'HOST': 'localhost',
+#         'PORT': '',
+#     }
+# }
+
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.postgresql',
-        'NAME': 'My_Achievements',
-        'USER': 'postgres',
-        'PASSWORD': '12345',
-        'HOST': 'localhost',
-        'PORT': '',
+        'ENGINE': 'django.db.backends.sqlite3',
+        'NAME': BASE_DIR / 'db.sqlite3',
     }
 }
 
